baed3f42a3a4936dae88bb840502c0bcecaa7213
belajarbarengrizal <rizalfahmi1511@gmail.com>
2022-08-05 10:23:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>Rizal Fahmi</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css" />
+</head>
+    <body>
+        <div id="wrap">
+            <div id="header">
+                <h1>Rizal Fahmi</h1>
+            </div>
+            <div class="row">
+                <div class="col-3 col-m-5">
+                    <div id="silebar">
+                        <div id="judul_silebar">
+                            SILEBAR
+                        </div>
+                        <div id="isi_silebar">
+                            <ul>
+                                <li><a href="#">Menu1</a></li>
+                                <li><a href="#">Menu2</a></li>
+                                <li><a href="#">Menu3</a></li>
+                                <li><a href="#">Menu4</a></li>
+                                <li><a href="#">Menu5</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-9 col-m-7">
+                    <div id="konten">
+                        <div class="col-12 col-m-12">
+                            <h2>Konten</h2>
+                           <p align="justify">Setelah membahas apa itu website dan sejarahnya, selanjutnya kami akan membahas jenis-jenis website.</p>
+
+<p align="justify">Secara garis besar, website dikelompokkan menjadi dua jenis, yaitu website dinamis dan website statis. Website dinamis memiliki konten yang berubah-ubah sesuai preferensi pengunjungnya, sementara website statis selalu menampilkan konten yang sama.</p>
+
+<p align="justify">Dari sini, jenis website dikelompokkan lagi menurut tujuan dan kegunaannya. Berikut jenis-jenis website yang ada saat ini:</p>
+
+<p align="justify">Website pribadi adalah jenis website yang dimiliki oleh orang pribadi atau individu. Tipe website seperti ini rata-rata merupakan website statis dengan ukuran yang lebih kecil dan bahasa pemrograman sederhana.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="row">
+                <div id="footer">
+                    <div id="text">
+                        Bagian Footer
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
